4fb862d14ea49ad74e8e4c2d59159fd327d0d828
Maciej Sawicki <sawik11@wp.pl>
2018-04-01 00:49:52 +0200

    Added board printing to a file

--- GameOfLife/file_manager.cpp
+++ GameOfLife/file_manager.cpp
@@ -1,9 +1,14 @@
 #include "stdafx.h"
 #include "file_manager.h"
+#include <ctime>
+#include <string>
+#include <windows.h>
+#include <cstdio>
+#pragma warning(disable:4996)
 
-void WritePGM(char* fname, int boardSize, bool** board) {
+void WritePGM(char* fname, bool** board, int boardSize) {
     FILE* file;
-        
+
     fopen_s(&file, fname, "wt");
     if (fname == NULL) {
         return;
@@ -13,4 +18,42 @@ void WritePGM(char* fname, int boardSize, bool** board) {
         fwrite(board[i], sizeof(bool), boardSize, file);
     }
     fclose(file);
+}
+
+// Get current date/time, format is YYYY-MM-DD.HH:mm:ss
+std::string currentDateTime() {
+    time_t now = time(0);
+    struct tm tstruct;
+    char buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%H.%M.%S", &tstruct);
+
+    return buf;
+}
+
+void saveBoard(bool** board, int boardSize, int iteration) {
+    std::string fileName = "screens";
+    createFolder(fileName);
+    fileName += "/" + currentDateTime();
+    createFolder(fileName);
+
+    if (iteration <= 0) {
+        fileName += "/begin.pgm";
+    } else {
+        fileName += "/iteration_" + std::to_string(iteration) + ".pgm";
+    }
+    WritePGM(_strdup(fileName.c_str()), board, boardSize);
+}
+
+void createFolder(std::string path) {
+    std::wstring stemp = std::wstring(path.begin(), path.end());
+    if (CreateDirectory(stemp.c_str(), NULL)) {
+        // Directory created
+    } else if (ERROR_ALREADY_EXISTS == GetLastError()) {
+        // Directory already exists
+    } else {
+        // Failed for some other reason
+    }
 }
--- GameOfLife/file_manager.h
+++ GameOfLife/file_manager.h
@@ -1,4 +1,11 @@
 ﻿#pragma once
+#include <string>
 
 
-void WritePGM(char* fname, int boardSize, bool** board);
+void WritePGM(char* fname, bool** board, int boardSize);
+
+std::string currentDateTime();
+
+void saveBoard(bool** board, int boardSize, int iteration);
+
+void createFolder(std::string path);
